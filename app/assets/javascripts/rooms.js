3391457a2cce792d4313d46bcc956f077917245c
Cleber Albuquerque <cleber@slatedev.com>
2017-02-10 16:54:20 -0700

    rooms page styled and ready for data

--- app/assets/javascripts/rooms.js
+++ app/assets/javascripts/rooms.js
@@ -1,5 +1,3 @@
-$( document ).ready(function() {
-	$('#calendar').fullCalendar({}); 
-  $('#calendar_1').fullCalendar({defaultView: 'listDay'});
-  $('#calendar_2').fullCalendar({defaultView: 'listDay'});
+$(document).ready(function() {
+	$('#calendar').fullCalendar({});
 });
